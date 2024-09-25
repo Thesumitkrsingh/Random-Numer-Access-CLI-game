c99df0be4ce8fb0da2b2d7b16ccf470fb73a2604
Thesumitkrsingh <textsumitsingh@gmail.com>
2024-09-25 22:27:08 +0530

    java_cli_random_number_guess_game

---
+++ src/RandomNumberGuess/Number.java
@@ -0,0 +1,63 @@
+package RandomNumberGuess;
+import java.util.*;
+
+public class Number {
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        System.out.println("Choose a Number Between 1 to 10");
+            int choice=scn.nextInt();
+            if(choice==1){
+                System.out.println("You lost the game");
+            }
+            else if(choice==2){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==3){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==4){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==5){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==6){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==7){
+                System.out.println("You win brother  congratulation");
+
+            }
+            else if(choice==8){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==9){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==10){
+                System.out.println("You Lost the game");
+
+            }
+            else if(choice==0){
+                System.out.println("Wrong Choice");
+
+            }
+            else{
+                System.out.println("you are not enter any number between 1 to 10");
+            }
+
+
+
+
+
+
+   }
+
+}
